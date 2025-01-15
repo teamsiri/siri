f26fbcfac9170a60a1808d5b6c3285f25466cfcd
teamsiri <siribot466@gmail.com>
2025-01-15 04:26:22 +0300

    Update logger.py

--- AnonXMusic/plugins/sudo/logger.py
+++ AnonXMusic/plugins/sudo/logger.py
@@ -1,5 +1,4 @@
 from pyrogram import filters
-
 from AnonXMusic import app
 from AnonXMusic.misc import SUDOERS
 from AnonXMusic.utils.database import add_off, add_on
@@ -8,12 +7,12 @@ from strings.filters import command
 
 @app.on_message(command(["تفعيل الاشعارات"]) & SUDOERS)
 @language
-async def enable_notifications(client, message):
+async def enable_notifications(client, message, _):  
     await add_on(2)
     await message.reply_text("تم تفعيل الإشعارات بنجاح!")
 
 @app.on_message(command(["تعطيل الاشعارات"]) & SUDOERS)
 @language
-async def disable_notifications(client, message):
+async def disable_notifications(client, message, _):  
     await add_off(2)
     await message.reply_text("تم تعطيل الإشعارات بنجاح!")
