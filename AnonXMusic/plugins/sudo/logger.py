5f33b838f3601c4eeb3ba359609a4310aa640434
teamsiri <siribot466@gmail.com>
2025-01-15 04:15:47 +0300

    Update logger.py

--- AnonXMusic/plugins/sudo/logger.py
+++ AnonXMusic/plugins/sudo/logger.py
@@ -5,19 +5,24 @@ from AnonXMusic.misc import SUDOERS
 from AnonXMusic.utils.database import add_off, add_on
 from AnonXMusic.utils.decorators.language import language
 
+from strings.filters import command
 
-@app.on_message(filters.command(["logger"]) & SUDOERS)
+@app.on_message(command(["تفعيل السجلات"]) & SUDOERS)
 @language
-async def logger(client, message, _):
+async def enable_notifications(client, message, _):
     usage = _["log_1"]
     if len(message.command) != 2:
         return await message.reply_text(usage)
-    state = message.text.split(None, 1)[1].strip().lower()
-    if state == "enable":
-        await add_on(2)
-        await message.reply_text(_["log_2"])
-    elif state == "disable":
-        await add_off(2)
-        await message.reply_text(_["log_3"])
-    else:
-        await message.reply_text(usage)
+    
+    await add_on(2)
+    await message.reply_text(_["log_2"])
+
+@app.on_message(command(["تعطيل السجلات"]) & SUDOERS)
+@language
+async def disable_notifications(client, message, _):
+    usage = _["log_1"]
+    if len(message.command) != 2:
+        return await message.reply_text(usage)
+    
+    await add_off(2)
+    await message.reply_text(_["log_3"])
